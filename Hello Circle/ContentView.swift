0c01ea01a5f0c657419d8a2c9b09bbdb346de2af
Adam Hartley <bytesguy@users.noreply.github.com>
2020-04-27 13:54:33 +0100

    Initial Commit

---
+++ Hello Circle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello Circle
+//
+//  Created by Adam Hartley on 20/04/2020.
+//  Copyright © 2020 CircleCI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
